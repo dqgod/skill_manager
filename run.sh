a9bb1cbce3394615239ee4733cd70f521221c99e
dangqi.dq <dangqi.dq@bytedance.com>
2026-05-20 21:30:06 +0800

    chore: update .gitignore and run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-if [ ! -f ".venv/bin/python" ]; then
-    echo "[ERROR] 虚拟环境不存在，请先运行: python -m venv .venv && .venv/bin/pip install -r requirements.txt"
-    exit 1
-fi
+# if [ ! -f ".venv/bin/python" ]; then
+#     echo "[ERROR] 虚拟环境不存在，请先运行: python -m venv .venv && .venv/bin/pip install -r requirements.txt"
+#     exit 1
+# fi
 
-.venv/bin/python -m src.main
+python -m src.main
